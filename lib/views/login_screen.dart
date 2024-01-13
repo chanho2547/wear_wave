2687131ef5a5a642a7b3b31a7b22f742df34dd53
CHAN <chanho2547@gmail.com>
2024-01-13 15:19:27 +0900

    로그인 버튼 생성

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -10,10 +10,27 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Material(
-        child: Center(
-          child: Text('Login Screen'),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.black87,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          width: 200,
+          height: 50,
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {},
+              child: const Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
